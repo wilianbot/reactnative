783d4442c62dd22429f216d2d961387b3802f9a9
wilianbot <wiliamrobaldossantos1@gmail.com>
2024-07-03 22:52:46 -0300

    Criando meus componentes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import Primeiro from './components/Primeiro'
+
+export default () => (
+<View>
+    <Text>{ 1 + 1 } </Text>
+    <Primeiro />
+</View>
+)
